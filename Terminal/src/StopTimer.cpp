8136e7a736d62985bc0a437257d2b27605e29011
Wilbert Alberts <wilbert.alberts@gmail.com>
2013-06-23 12:16:28 +0200

    Add StopTimer.

---
+++ Terminal/src/StopTimer.cpp
@@ -0,0 +1,42 @@
+/*
+ * StopTimer.cpp
+ *
+ *  Created on: Jun 22, 2013
+ *      Author: wilbert
+ */
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+#include "DoubleBuffer.h"
+#include "Parameter.h"
+
+#include "StopTimer.h"
+
+
+
+const std::string StopTimer::stopTimerCommand("stopTimer");
+
+
+StopTimer::StopTimer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StopTimer::~StopTimer() {
+	// TODO Auto-generated destructor stub
+}
+
+void StopTimer::execStopTimer(DoubleBuffer* db, int argc, char* argv[])
+{
+	if (argc!=1) {
+		std::cerr << "Usage: "<< stopTimerCommand << std::endl;
+		exit(-1);
+	}
+
+	// TODO: replace hardcoded parameter id by reference.
+	Parameter* par = new Parameter(db, "PeriodicTimer.stopRunning");
+	par->set(1.0);
+}
+
---
+++ Terminal/src/StopTimer.h
@@ -0,0 +1,23 @@
+/*
+ * StopTimer.h
+ *
+ *  Created on: Jun 22, 2013
+ *      Author: wilbert
+ */
+
+#ifndef STOPTIMER_H_
+#define STOPTIMER_H_
+
+class DoubleBuffer;
+
+class StopTimer {
+public:
+	StopTimer();
+	virtual ~StopTimer();
+
+	static void execStopTimer(DoubleBuffer* db, int argc, char* argv[]);
+
+	static const std::string stopTimerCommand;
+};
+
+#endif /* STOPTIMER_H_ */
