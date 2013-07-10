aad37371fe9d25b62e21846ef0d4a84b9eee9f5b
Wilbert Alberts <wilbert.alberts@gmail.com>
2013-07-10 17:08:44 +0200

    Create Devices class.
    	Devices translates raw SPI registers into real world SI values.

---
+++ Servo/src/Devices.cpp
@@ -0,0 +1,91 @@
+/*
+ * Devices.cpp
+ *
+ *  Created on: Jul 10, 2013
+ *      Author: wilbert
+ */
+
+#include "Devices.h"
+
+#include "SPI.h"
+#include "Parameter.h"
+#include "PeriodicTimer.h"
+
+Devices::Devices() :
+		spi(SPI::getInstance()), pt(PeriodicTimer::getInstance()), par_rawAngle(
+				new Parameter("Dev.rawAngle")), par_angle(
+				new Parameter("Dev.angle")), par_angleV(
+				new Parameter("Dev.angleV")), par_angleA(
+				new Parameter("Dev.angleA")), par_angleGain(
+				new Parameter("Dev.angleGain")), par_angleOffset(
+				new Parameter("Dev.angleOffset")) {
+}
+
+Devices::~Devices() {
+	// TODO Auto-generated destructor stub
+}
+
+void Devices::sampleAngle(double frequency) {
+	// Start with shifting previous values
+	angle[2] = angle[1];
+	angle[1] = angle[0];
+	angle_v[1] = angle_v[0];
+	// determine raw angle
+	double hpos = spi->getRegister(SPI::HEIGHT1);
+	double npos = spi->getRegister(SPI::HEIGHT2);
+	double raw_angle = hpos - npos;
+	par_rawAngle->set(raw_angle);
+	// calculate new angle with gain and offset
+	angle[0] = raw_angle * par_angleGain->get() + par_angleOffset->get();
+	angle_v[0] = (angle[0] - angle[1]) * frequency;
+	angle_v[1] = (angle[1] - angle[2]) * frequency;
+	angle_a[0] = (angle_v[0] - angle_v[1]) * frequency;
+	// store calculated values into parameters
+	par_angle->set(angle[0]);
+	par_angleV->set(angle_v[0]);
+	par_angleA->set(angle_a[0]);
+}
+
+void Devices::sample() {
+	double frequency = pt->getFrequency();
+
+	sampleAngle(frequency);
+	samplePosition(frequency);
+}
+
+void Devices::samplePosition(double frequency) {
+	// Start with shifting previous values
+	pos[2] = pos[1];
+	pos[1] = pos[0];
+	pos_v[1] = pos_v[0];
+
+	unsigned int rawpos = static_cast<unsigned int>(spi->getRegister(
+			SPI::ENCPOS));
+	// Mask relevant bits
+	rawpos = rawpos & 0x0fff; // 12 bits
+	// determine delta w.r.t. previous sample;
+	int delta = rawpos - prevRawPos;
+	// store new value for prevRawPos
+	prevRawPos = rawpos;
+	int nrIncs = par_nrIncrements->get();
+
+	// Correct for over/underruns
+	if (delta > nrIncs / 2) {
+		delta -= nrIncs;
+	} else {
+		if (delta < -nrIncs / 2) {
+			delta += nrIncs;
+		}
+	}
+
+	// Update encoder position
+	encPos += delta;
+
+	// Transform into real position
+	pos[0] = encPos * par_posGain->get() + par_posOffset->get();
+
+	// Calculate velocity and acceleration
+	pos_v[0] = (pos[0] - pos[1]) * frequency;
+	pos_v[1] = (pos[1] - pos[2]) * frequency;
+	pos_a[0] = (pos_v[0] - pos_v[1]) * frequency;
+}
---
+++ Servo/src/Devices.h
@@ -0,0 +1,64 @@
+/*
+ * Devices.h
+ *
+ *  Created on: Jul 10, 2013
+ *      Author: wilbert
+ */
+
+#ifndef DEVICES_H_
+#define DEVICES_H_
+
+class Parameter;
+class SPI;
+class PeriodicTimer;
+
+class Devices {
+public:
+	static Devices* getInstane();
+	virtual ~Devices();
+
+	void sample();
+	static void sample(void* context);
+
+private:
+	Devices();
+	void sampleAngle(double frequency);
+	void samplePosition(double frequency);
+
+	static Devices* instance;
+
+	SPI* spi;
+	PeriodicTimer* pt;
+
+	double angle[3];
+	double angle_v[2];
+	double angle_a[1];
+
+	Parameter* par_rawAngle;
+	Parameter* par_angle;
+	Parameter* par_angleV;
+	Parameter* par_angleA;
+	Parameter* par_angleGain;
+	Parameter* par_angleOffset;
+
+	double pos[3];
+	double pos_v[2];
+	double pos_a[1];
+
+	unsigned int prevRawPos;
+	int encPos;
+
+	Parameter* par_rawPos;
+	Parameter* par_pos;
+	Parameter* par_posV;
+	Parameter* par_posA;
+	Parameter* par_posGain;
+	Parameter* par_posOffset;
+
+	Parameter* par_nrIncrements;
+
+
+
+};
+
+#endif /* DEVICES_H_ */
