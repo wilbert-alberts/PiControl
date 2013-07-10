78d53b44e448d948056fe6f25d478b965e40de67
Wilbert Alberts <wilbert.alberts@gmail.com>
2013-07-10 13:52:18 +0200

    Enhance output of timing statistics.

--- Terminal/src/CmdDumpTiming.cpp
+++ Terminal/src/CmdDumpTiming.cpp
@@ -34,14 +34,24 @@ void CmdDumpTiming::execute(std::list<std::string>& args) {
 	Parameter* minMargin;
 	Parameter* maxMargin;
 	Parameter* margin;
+	Parameter* frequency;
 
 	minMargin = new Parameter(TimeStats::par_minMargin);
 	maxMargin = new Parameter(TimeStats::par_maxMargin);
 	margin = new Parameter(TimeStats::par_margin);
+	frequency= new Parameter(TimeStats::par_frequency);
 
-	std::cout << "margin:    " << margin->get() << std::endl;
-	std::cout << "minMargin: " << minMargin->get() << std::endl;
-	std::cout << "maxMargin: " << maxMargin->get() << std::endl;
+	std::cout << "frequency: " << frequency->get() << "Hz (period: " << 1.0/frequency->get()<< " seconds)" << std::endl;
 
+
+	unsigned int periodInUs = 1000000/frequency->get();
+	unsigned int actualPeriod = periodInUs - margin->get();
+	unsigned int smallestPeriod = periodInUs - maxMargin->get();
+	unsigned int largestPeriod = periodInUs - minMargin->get();
+
+
+	std::cout << "actual period:   " << actualPeriod/1000.0 << "ms ("<< 100.0*actualPeriod/periodInUs << "%)" << std::endl;
+	std::cout << "largest period:  " << largestPeriod/1000.0 << "ms ("<< 100.0*largestPeriod/periodInUs << "%)" << std::endl;
+	std::cout << "smallest period: " << smallestPeriod/1000.0 << "ms ("<< 100.0*smallestPeriod/periodInUs << "%)" << std::endl;
 }
 
