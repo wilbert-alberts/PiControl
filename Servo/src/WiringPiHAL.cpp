28e4ce77e99a087073f946543611c41ba3edd0c3
Wilbert Alberts <wilbert.alberts@gmail.com>
2013-09-28 18:46:17 +0200

    Create 'get in sync' protocol for PI and MBED.

--- Servo/src/WiringPiHAL.cpp
+++ Servo/src/WiringPiHAL.cpp
@@ -41,11 +41,61 @@ int WiringPiHAL::digitalRead(int pin) {
 void WiringPiHAL::digitalWrite(int pin, int value) {
 	::digitalWrite(pin, value);
 }
+
 void WiringPiHAL::wiringPiSPIDataRW(int channel, unsigned char *data, int len) {
-	//::wiringPiSPIDataRW(channel, data, len);
+
+	// Run protocol to get rid of remaining data in SPI FIFO
+	typedef enum {
+		INIT,
+		HALFWAY,
+		EXIT
+	} State_enum;
+
+	State_enum state = INIT;
+	unsigned char buffer;
+	do {
+		switch (state) {
+		case INIT:
+			buffer = 0xAA;
+			::wiringPiSPIDataRW(channel, &buffer, 1);
+			switch (buffer) {
+			case 0xAA:
+				state = HALFWAY;
+				break;
+			default:
+				state = INIT;
+				break;
+			}
+			break;
+		case HALFWAY:
+			buffer = 0xBB;
+			::wiringPiSPIDataRW(channel, &buffer, 1);
+			switch (buffer) {
+			case 0xAA:
+				state = HALFWAY;
+				break;
+			case 0xBB:
+				state = EXIT;
+				break;
+			default:
+				state = INIT;
+				break;
+			}
+			break;
+		default:
+			// Should not be reached
+			std::clog << "This code should not be reached: " << std::endl;
+			break;
+		}
+	} while (state != EXIT);
+
+	// MBED en PI in sync, no communicate buffer.
 	for (int i=0; i<len; i++) {
 		::wiringPiSPIDataRW(channel, data+i, 1);
 	}
+
 }
+
+
 
 #endif
