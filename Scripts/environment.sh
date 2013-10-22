cb8443d8f42453acfb0d9fae93ff7d67edee4cd3
ben <ben@ben-kubuntu-11.10>
2013-10-22 23:21:53 +0200

    Add setPWM alias.

--- Scripts/environment.sh
+++ Scripts/environment.sh
@@ -3,9 +3,11 @@
 SERVO=/home/pi/PiControl/Servo/ARM-Debug/Servo
 TERMINAL=/home/pi/PiControl/Terminal/ARM-Debug/Terminal
 
+alias terminal="$TERMINAL"
 alias dumpParameters="$TERMINAL dumpParameters"
 alias addTrace="$TERMINAL addTrace"
 alias delTrace="$TERMINAL delTrace"
 alias dumpTraces="$TERMINAL dumpTraces"
 alias freq1="$TERMINAL PeriodicTimer.frequency 1"
 alias freq100="$TERMINAL PeriodicTimer.frequency 100"
+alias setPWM="$TERMINAL Dev.dutycycle"
