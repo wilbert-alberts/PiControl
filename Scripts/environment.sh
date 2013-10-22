41d2d01eba8a027dad63a325fe3721a2193ac9c1
ben <ben@ben-kubuntu-11.10>
2013-10-22 23:04:11 +0200

    Add additional aliases.

--- Scripts/environment.sh
+++ Scripts/environment.sh
@@ -4,3 +4,8 @@ SERVO=/home/pi/PiControl/Servo/ARM-Debug/Servo
 TERMINAL=/home/pi/PiControl/Terminal/ARM-Debug/Terminal
 
 alias dumpParameters="$TERMINAL dumpParameters"
+alias addTrace="$TERMINAL addTrace"
+alias delTrace="$TERMINAL delTrace"
+alias dumpTraces="$TERMINAL dumpTraces"
+alias freq1="$TERMINAL PeriodicTimer.frequency 1"
+alias freq100="$TERMINAL PeriodicTimer.frequency 100"
