4365051a89cc2cde74c3190adc83818031a44177
ben <ben@ben-kubuntu-11.10>
2013-11-19 21:22:21 +0100

    Generate trace plot script.

---
+++ Scripts/plotTrace.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import subprocess
+
+import string
+
+
+output = subprocess.check_output (["ssh", "root@192.168.1.40", "/home/pi/PiControl/Terminal/ARM-Debug/Terminal dumpTraces"])
+
+lines = string.split(output,'\n')
+
+traceOut = open('/tmp/trace.txt','w')
+traceOut.write(output)
+traceOut.close()
+
+
+fields=string.split(string.replace(lines[0], "# Samplecounter", ""))
+
+
+idx=2
+datasets=[]
+for f in fields:
+	datasets.append('"/tmp/trace.txt" using 1:'+str(idx)+' with lines title "' + f + '" ')
+	idx=idx+1
+
+cmd="plot " + string.join(datasets, ",")
+
+outfile = open('/tmp/genplot.p','w')
+outfile.write(cmd+'\n')
+outfile.close()
+
+
+subprocess.call(["gnuplot", "-persist", "/tmp/genplot.p"])
+
